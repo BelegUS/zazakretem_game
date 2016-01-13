8f1d31e60ef712bc1c0eff747b6d90eb65c25c68
Szymon Cendrzak <belegussuicune@gmail.com>
2016-01-13 18:05:10 +0100

    Menu using KnpMenuBundle added.

---
+++ src/ZaZakretem/GameBundle/Menu/Builder.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BelegUS
+ * Date: 2016-01-09
+ * Time: 01:02
+ */
+
+namespace ZaZakretem\GameBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class Builder implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+
+        $menu->setChildrenAttribute('class', 'sidebar-menu');
+        $menu->addChild('Dash', array('route' => '_index'));
+
+//        // access services from the container!
+//        $em = $this->container->get('doctrine')->getManager();
+//        // findMostRecent and Blog are just imaginary examples
+//        $blog = $em->getRepository('AppBundle:Blog')->findMostRecent();
+
+//        $menu->addChild('Latest Blog Post', array(
+//            'route' => 'blog_show',
+//            'routeParameters' => array('id' => $blog->getId())
+//        ));
+
+        // create another menu item
+        $menu->addChild('Auctions', array('route' => 'view_auctions'));
+        // you can also add sub level's to your menu's as follows
+//        $menu['About Me']->addChild('Edit profile', array('route' => 'edit_profile'));
+
+        // ... add more children
+
+        return $menu;
+    }
+}
